0134619783cc53dd148267de271d4db320eabb60
Tomasz McDowell <mcdowelltomasz@gmail.com>
2021-12-07 00:42:45 -0800

    complete exercises up to 1.2

---
+++ part1/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  )
+}
+
+export default App
